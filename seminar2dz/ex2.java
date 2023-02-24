55843daf1e9ab7de537790c3783cf4b3d01fc08c
Anton23 <antonavdonin87@gmail.com>
2023-02-24 20:58:58 +0300

    dz2 log

--- seminar2dz/ex2.java
+++ seminar2dz/ex2.java
@@ -2,6 +2,68 @@
 // Реализуйте алгоритм сортировки пузырьком числового массива, 
 // результат после каждой итерации запишите в лог-файл.
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.LogManager;
+
 public class ex2 {
-    
+
+    public static void logFile(String[] args) {
+        try {
+            LogManager.getLogManager().readConfiguration(
+                    logging.class.getResourceAsStream("/logging.properties"));
+        } catch (IOException e) {
+            System.err.println("Could not setup logger configuration: " + e.toString());
+        }
+    }  
+    public static void main(String[] args) {
+        int [] mas = {11, 3, 14, 16, 7};
+ 
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length-1; i++) {
+                if(mas[i] > mas[i+1]){
+                    isSorted = false;
+ 
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mas));
+    } 
 }
+
+// class Test {
+//     public static void main(String arg[]) {
+//         PrintWriter log;
+//         FileWriter logFile;
+//         /*  Сначала откроем файл, в который будем писать ошибки */
+//         try {
+//             logFile = new FileWriter("exceptions.log", true);
+//             log = new PrintWriter((java.io.Writer)logFile);
+//         } catch (IOException ex) {
+//             ex.printStackTrace();
+//             return;
+//         }
+//         try {
+//         /*  Тут находится основной код вашего приложения. Ошибки будут
+//             проявляться в виде исключений, например так (искуственный
+//             пример)
+//         */
+//             throw new Exception();
+//         } catch (Exception ex) {
+//         /*  Перехватываем все необработанные исключения и пишем в логфайл
+//             временную отметку, сообщение об ошибке и стектрейс (в котором
+//             будут указаны методы, которые привели к ошибке и номера строк
+//             в исходниках)
+//         */
+//             log.printf("\n%s: %s\n", LocalDateTime.now(), ex.getMessage());
+//             ex.printStackTrace(log);
+//             log.flush();
+//         }
+//     }
+// }
